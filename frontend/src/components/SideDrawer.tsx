787824edb8cb160f04f34588a7d39a1be7d4cf39
Kshitij <kshitij.tanwar2002@gmail.com>
2024-05-23 18:15:05 +0530

    chore: Added form for event

--- frontend/src/components/SideDrawer.tsx
+++ frontend/src/components/SideDrawer.tsx
@@ -5,11 +5,10 @@ import MuiDrawer from "@mui/material/Drawer";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import List from "@mui/material/List";
-
+import EventForm from "./EventForm";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItem from "@mui/material/ListItem";
@@ -19,7 +18,7 @@ import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 
-const drawerWidth = 240;
+const drawerWidth = 220;
 
 const openedMixin = (theme: Theme): CSSObject => ({
     width: drawerWidth,
@@ -92,7 +91,7 @@ const Drawer = styled(MuiDrawer, {
 
 const SideDrawer = () => {
     const [open, setOpen] = React.useState(false);
-
+    const [isEventFormOpen, setEventFormIsOpen] = React.useState(true);
     const handleDrawer = () => {
         setOpen(!open);
     };
@@ -100,7 +99,7 @@ const SideDrawer = () => {
     return (
         <Box sx={{ display: "flex" }}>
             <AppBar position="fixed" open={open}></AppBar>
-            <Drawer variant="permanent" open={open}>
+            <Drawer variant="permanent" open={open} className="z-10">
                 <DrawerHeader>
                     <IconButton onClick={handleDrawer}>
                         {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
@@ -120,6 +119,7 @@ const SideDrawer = () => {
                                     justifyContent: open ? "initial" : "center",
                                     px: 2.5,
                                 }}
+                                onClick={()=>setEventFormIsOpen(true)}
                             >
                                 <ListItemIcon
                                     sx={{
@@ -128,7 +128,7 @@ const SideDrawer = () => {
                                         justifyContent: "center",
                                     }}
                                 >
-                                    <IoIosAddCircleOutline className="text-2xl"/>
+                                    <IoIosAddCircleOutline className="text-2xl" />
                                 </ListItemIcon>
                                 <ListItemText
                                     primary={text}
@@ -178,21 +178,7 @@ const SideDrawer = () => {
             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
                 <DrawerHeader />
                 <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Rhoncus dolor purus non enim praesent elementum
-                    facilisis leo vel. Risus at ultrices mi tempus imperdiet.
-                    Semper risus in hendrerit gravida rutrum quisque non tellus.
-                    Convallis convallis tellus id interdum velit laoreet id
-                    donec ultrices. Odio morbi quis commodo odio aenean sed
-                    adipiscing. Amet nisl suscipit adipiscing bibendum est
-                    ultricies integer quis. Cursus euismod quis viverra nibh
-                    cras. Metus vulputate eu scelerisque felis imperdiet proin
-                    fermentum leo. Mauris commodo quis imperdiet massa
-                    tincidunt. Cras tincidunt lobortis feugiat vivamus at augue.
-                    At augue eget arcu dictum varius duis at consectetur lorem.
-                    Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                    sapien faucibus et molestie ac.
+                    <EventForm setFunction={setEventFormIsOpen} parameter={isEventFormOpen}/>
                 </Typography>
             </Box>
         </Box>
