b4bb9700bd8377d4d04d06dd28c634cf64593fd7
Kshitij <kshitij.tanwar2002@gmail.com>
2024-06-17 20:19:59 +0530

    refactor: Add 'id' field to SubEventSerializer fields

--- user/serializers.py
+++ user/serializers.py
@@ -35,7 +35,7 @@ class ProfileSerializer(serializers.ModelSerializer):
 class SubEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubEvent
-        fields = ['event', 'name', 'start_datetime', 'end_datetime', 'venue_name', 'venue_location', 'venue_capacity', 'capacity']
+        fields = ['id','event', 'name', 'start_datetime', 'end_datetime', 'venue_name', 'venue_location', 'venue_capacity', 'capacity']
 
 class EventSerializer(serializers.ModelSerializer):
 
